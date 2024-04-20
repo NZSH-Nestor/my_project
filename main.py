0b05bb3739c806fa2140cd02f43cde63127d799a
NZSH-Nestor <167652494+NZSH-Nestor@users.noreply.github.com>
2024-04-20 17:24:39 -0500

    Create main.py
    palabras modernas

---
+++ main.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROLF":"Una respuesta a una broma",
+            "SHEESH":"Ligera desaprobación",
+            "CREEPY":"Aterrador, siniestro",
+            "AGGRO":"Ponerse agresivo/enojado"
+            }
+            
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("la palabra no se encontró")
